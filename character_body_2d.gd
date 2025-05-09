47e2d4c9cada325fc0422fb7a3f641b27f01cc76
hgrnjkhg <205313624+hgrnjkhg@users.noreply.github.com>
2025-05-09 13:31:00 +0200

    nivell ari

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,1 +1,18 @@
 extends CharacterBody2D
+
+var velocitat := 300
+var direccio := Vector2.ZERO
+
+func _ready() -> void:
+	position = Vector2(500, 300) #possició inicial
+	
+func _process(delta: float) -> void:
+	direccio.x = Input.get_axis("dreta","esquerre")
+	direccio.y = Input.get_axis("davant","darrere")
+	
+	velocity.x = direccio.x * velocitat
+	velocity.y = direccio.y * velocitat
+	
+	
+	var desplaçament: Vector2 = direccio * velocitat
+	position = position + desplaçament*delta
