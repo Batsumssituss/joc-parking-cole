26862daeb3c1126d94b4235f209004d0a0bff0a9
hgrnjkhg <205313624+hgrnjkhg@users.noreply.github.com>
2025-05-12 13:37:12 +0200

    script del cotxe + collisions del circuit
    script pq el cotxe es mogui i fagis be curvas i posar collisions en forma de rectangle per anar marcanmt el circuit

---
+++ area_2d.gd
@@ -0,0 +1,10 @@
+extends Area2D
+func _ready() -> void:
+	pass
+	
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	visible = false
+	
--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,18 +1,35 @@
-extends CharacterBody2D
+#extends CharacterBody2D
 
-var velocitat := 300
-var direccio := Vector2.ZERO
+#var velocitat := 300
+#var direccio := Vector2.ZERO
 
-func _ready() -> void:
-	position = Vector2(500, 300) #possició inicial
+#func _ready() -> void:
+#	position = Vector2(500, 300) #possició inicial
 	
-func _process(delta: float) -> void:
-	direccio.x = Input.get_axis("dreta","esquerre")
-	direccio.y = Input.get_axis("davant","darrere")
+#func _process(delta: float) -> void:
+#	direccio.x = Input.get_axis("dreta","esquerre")
+#	direccio.y = Input.get_axis("davant","darrere")
 	
-	velocity.x = direccio.x * velocitat
-	velocity.y = direccio.y * velocitat
+#	velocity.x = direccio.x * velocitat
+#	velocity.y = direccio.y * velocitat
 	
 	
-	var desplaçament: Vector2 = direccio * velocitat
-	position = position + desplaçament*delta
+#	var desplaçament: Vector2 = direccio * velocitat
+#	position = position + desplaçament*delta
+
+extends CharacterBody2D 
+
+var speed = 400 
+var rotation_speed = 1.5 
+var rotation_direction = 0 
+
+ 
+func get_input(): 
+	rotation_direction = Input.get_axis("esquerra", "dreta") 
+
+	velocity = transform.y * Input.get_axis("adalt", "abaix") * speed 
+
+func _physics_process(delta): 
+	get_input() 
+	rotation += rotation_direction * rotation_speed * delta 
+	move_and_slide() 
